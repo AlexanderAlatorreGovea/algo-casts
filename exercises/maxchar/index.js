9cdf2cf9ae1324295025fb8be7926285fb6b5861
Alexander Govea <alexander_alatorre1993@hotmail.com>
2021-08-19 06:52:59 -0600

    Solved chunked, fizzbuzz, and maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -6,25 +6,25 @@
 // maxChar("apple 1231111") === "1"
 
 function maxChar(str) {
-    const charMap = {};
-    let max = 0;
-    let maxChar = "";
+  const charMap = {};
+  let max = 0;
+  let maxChar = "";
 
-    for(let char of str) {
-        if(!charMap[char]) {
-            charMap[char] = 1
-        } else {
-            charMap[char]++
-        }
+  for (let char of str) {
+    if (charMap[char]) {
+        charMap[char]++;
+    } else {
+        charMap[char] = 1;    }
+  }
+
+  for (let char in charMap) {
+    if (charMap[char] > max) {
+        max = charMap[char];
+        maxChar = char;
     }
+  }
 
-    for(let char in charMap) {
-        if(charMap[char] > max) {
-          max = charMap[char];
-          maxChar = char
-        }
-    } 
-    return maxChar
+  return maxChar;
 }
 
 module.exports = maxChar;
