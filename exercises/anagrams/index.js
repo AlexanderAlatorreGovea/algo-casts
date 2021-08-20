be09347c15b59ca49009edd3daa69741d8f5f953
Alexander Govea <alexander_alatorre1993@hotmail.com>
2021-08-20 09:53:25 -0600

    Solved anagrams and chunked problems

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,39 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+//ignore space and punctuations with 
+//word.replace(/[^\w]/g, "").toLowerCase()
+
+function objectCharacterMap(string) {
+    let obj = {};
+    const replacedStr = string.replace(/[^\w]/g, "").toLowerCase();
+    
+    for(let char of replacedStr) {
+        if(!obj[char]) {
+            obj[char] = 1
+        } else {
+            obj[char]++
+        }
+    }
+
+    return obj
+}
+
+function anagrams(stringA, stringB) {
+    const stringOne = objectCharacterMap(stringA);
+    const stringTwo = objectCharacterMap(stringB);
+
+    if(Object.keys(stringOne).length !== Object.keys(stringTwo).length) {
+        return false
+    }
+ 
+    for(let char in stringOne) {
+        if(stringOne[char] === stringTwo[char]) {
+            return true
+        }
+    }
+
+    return false
+}
 
 module.exports = anagrams;
--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,18 +8,33 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {
-  const chunked = [];
+// function chunk(array, size) {
+//   const chunked = [];
 
-  for (let element of array) {
-    const last = chunked[chunked.length -1]; //last element in the chunked array so first time we will get undefined
-    if(!last || last.length  === size) {//first time is undefined and we will fall into this statement
-        chunked.push([element]) // this line of code says that we will push a new chunck into the chunked with the currrent element we will iterate over which is one on first loop
-    } else {
-        last.push(element) //since the last element exists then we push the next element in the array which is 2
-    }// finally we will iterate to the last element of the array then we chesk if the length is equal to the chunk size which it is because it created a new chunk and add 3
-   }
-   return chunked
-} 
+//   for(let element of array) {
+//     const last = chunked[chunked.length -1]; //last element in the chunked array so first time we will get undefined
+//     if(!last || last.length  === size) {//first time is undefined and we will fall into this statement
+//         chunked.push([element]) // this line of code says that we will push a new chunck into the chunked with the currrent element we will iterate over which is one on first loop
+//     } else {
+//         last.push(element) //since the last element exists then we push the next element in the array which is 2
+//     }// finally we will iterate to the last element of the array then we chesk if the length is equal to the chunk size which it is because it created a new chunk and add 3
+//    }
+//    return chunked
+// } 
+
+function chunk(array, size) {
+    const chunked = [];
+
+    for(let element of array) {
+        const last = chunked[chunked.length - 1];
+
+        if(!last || last.length == size) {
+            chunked.push([element])
+        } else {
+            last.push(element)
+        }
+    }
+    return chunked
+}
 
 module.exports = chunk;
