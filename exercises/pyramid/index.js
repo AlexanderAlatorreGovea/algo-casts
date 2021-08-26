fc28a7f5a939f3d3490539cb43a667c91e281218
Alexander Govea <alexander_alatorre1993@hotmail.com>
2021-08-25 18:57:32 -0600

    Continued working on the matrix problem

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -32,21 +32,21 @@ function pyramid(n) {
                 level += ' '
             }
         }
-        console.log(level)
+        console.log(midPoint)
     }
 
-    // const midPoint = Math.floor((n * 2 - 1) / 2);
-    // for(let row = 0; row < n; row++) {
-    //     let level = '';
-    //     for(let column = 0; column < n * 2 - 1; column++) {
-    //         if(midPoint - row <= column && midPoint + row >= column) {
-    //             level += '#'
-    //           } else {
-    //             level += ' '
-    //           }
-    //     }
-    //     console.log(level)
-    // }
+    const midPoint = Math.floor((n * 2 - 1) / 2);
+    for(let row = 0; row < n; row++) {
+        let level = '';
+        for(let column = 0; column < n * 2 - 1; column++) {
+            if(midPoint - row <= column && midPoint + row >= column) {
+                level += '#'
+              } else {
+                level += ' '
+              }
+        }
+        console.log(level)
+    }
 }
 
 module.exports = pyramid;
